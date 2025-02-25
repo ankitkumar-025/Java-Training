e8df7e4fc724847965a1fc824fd03fbd132ee311
Ankit kumar gupta <ak0199374@gmail.com>
2025-02-25 17:24:41 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,15 @@
+public class Pattern7 {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++){
+            for(int j=1;j<=7;j++){
+                if(j==4||(i==1 && j<=4)||(i==7&&j>=4)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
