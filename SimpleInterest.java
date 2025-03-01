10416ac88b71fccbbc6daae6faade40c7bf6789b
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-01 22:46:32 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Principle Amount:");
+        double p=sc.nextDouble();
+        System.out.print("Enter Rate:");
+        int r=sc.nextInt();
+        System.out.print("Enter Time:" );
+        int t=sc.nextInt();
+        double SI=(p*r*t)/100;
+        System.out.print("Simple_interest="+SI);
+        sc.close();
+    }
+}
