b328de76d3b4e94e78ebffc9d8e852bb02be84d8
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-03 17:38:07 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter any number:");
+        int number=sc.nextInt();
+        if(number%2==0){
+            System.out.print(number +" is even");
+        }
+        else{
+            System.out.print(number +" is odd");
+        }
+       sc.close();;
+
+    }
+}
