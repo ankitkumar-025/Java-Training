289981dc4e8549fe3fcb4ad146108b31000e8e71
Ankit kumar gupta <ak0199374@gmail.com>
2025-03-25 20:21:36 +0530

    Add files via upload

---
+++ Code.java
@@ -0,0 +1,22 @@
+public class Code {
+    public static void main(String[] args) {
+        Employee obj=new Employee(101,"Ankit",35000);
+        System.out.print(obj);
+    }
+}
+class Employee{
+    private int id;
+    private String name;
+    private double salary;
+    public Employee(int id,String name,double salary){
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+    public String toString(){
+        return this.id+","+this.name+","+this.salary;
+    }
+}
+
+    
+
