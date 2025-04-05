e00eb530e1c3b89caa25aece74073fe1165fa9a7
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-06 01:10:17 +0530

    Update Pattern7.java

--- Pattern7.java
+++ Pattern7.java
@@ -8,7 +8,7 @@ public class Pattern7 {
                 else{
                     System.out.print(" ");
                 }
-            }pppn
+            }
             System.out.println();
         }
     }
