271c03fe152396097303d13573c2eeecd628fe09
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-07 00:34:55 +0530

    Update Pattern14.java

--- Pattern14.java
+++ Pattern14.java
@@ -1,7 +1,7 @@
 public class Pattern14 {
     public static void main(String[] args) {
          for(int i=1;i<=5;i++){
-             for(int j=i;j>=1;j--){
+             for(int j=i;j>=1;j--){i
                  System.out.print(j);
              }
              System.out.println();
