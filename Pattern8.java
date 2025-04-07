9ee02ce225587385205babd373d2c7cc1197b048
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-08 00:57:19 +0530

    Update Pattern8.java

--- Pattern8.java
+++ Pattern8.java
@@ -8,7 +8,7 @@ public class Pattern8 {
                 else{
                     System.out.print(" ");
                 }f
-            }fg
+            }fgg
             System.out.println();
         }
     }
