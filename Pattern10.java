77098ee10356c02286a6050701cc2f30a83259bd
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-15 00:06:18 +0530

    Update Pattern10.java

--- Pattern10.java
+++ Pattern10.java
@@ -6,7 +6,7 @@ public class Pattern10 {
                     System.out.print(" *");
                 }
                 else{
-                    System.out.print("  ");bba
+                    System.out.print("  ");bbaa
                 }
             }
             System.out.println();
