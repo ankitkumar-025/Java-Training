ee991c43a1722025dc3141f4932de81238167982
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-17 23:26:25 +0530

    Update Pattern12.java

--- Pattern12.java
+++ Pattern12.java
@@ -8,7 +8,7 @@ public class Pattern12 {
                 else{
                     System.out.print(" ");
                     
-                }kkp
+                }
             }
             System.out.println();
         }
