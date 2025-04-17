f47ec0103c62bd9c87bb4beec2dff1640c80aa7d
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-18 00:02:37 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -5,7 +5,7 @@ public class OddEven {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter any number:");
         int number=sc.nextInt();
-        if(number%2==0){
+        if(number%2==0){jj
             System.out.print(number +" is even");
         }
         else{
