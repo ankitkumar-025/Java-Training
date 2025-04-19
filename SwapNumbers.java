85f549319b867954345dde94a7e4375dbbf742b1
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-19 12:52:09 +0530

    Update SwapNumbers.java

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -4,7 +4,7 @@ public class SwapNumbers {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter first number:");
-        int a=sc.nextInt();zaa
+        int a=sc.nextInt();zaaz
         System.out.print("Enter second number:");
         int b=sc.nextInt();
         System.out.println("Before swapping: a="+ a +", b=" + b);
