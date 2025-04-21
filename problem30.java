60ec673ee5b7fd4830d8cf9bdf330f8355c9b133
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-22 01:45:46 +0530

    Update problem30.java

--- problem30.java
+++ problem30.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class problem30 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the last no: ");
+        System.out.print("Enter the last no: ");f
         int n = sc.nextInt();
         factorial(n);
     }
