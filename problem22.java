38e420086cca7753ebbaf460babae39eac7ab3af
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-25 01:23:39 +0530

    Update problem22.java

--- problem22.java
+++ problem22.java
@@ -2,7 +2,7 @@
     import java.util.Scanner;
 
 public class problem22 {
-    public static void main(String[] args) {gss
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the 1st No.");
         int x = sc.nextInt();
