3cce6a829a6df2f5b8c41880ecbaa77c3e23b6ba
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-27 01:55:02 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -3,7 +3,7 @@ public class Pattern {
         for (int i = 1; i <= 7; i++) {
             for (int j = 1; j <= 7; j++) {
                 if (i == 1 || i == 7 || j == 1 || j == 7) {
-                    System.out.print("*");
+                    System.out.print("*");hhh
                 } else {
                     System.out.print(" ");
                 }
