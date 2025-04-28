071c8ba332da1eacca7ae69ca49da68c0955c614
Ankit kumar gupta <ak0199374@gmail.com>
2025-04-28 22:42:07 +0530

    Update Pattern10.java

--- Pattern10.java
+++ Pattern10.java
@@ -1,7 +1,7 @@
 public class Pattern10 {
     public static void main(String[] args) {
         for(int i=1;i<=7;i++){
-            for(int j=1;j<=7;j++){aaaaa
+            for(int j=1;j<=7;j++){aaaaaaaa
                 if((j==4&&i>=4)||(i==j&&j<=4)||(i+j==8&&j>=4)){
                     System.out.print(" *");
                 }
