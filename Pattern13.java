ebf19efa0a567c62922934d8f6b1c439908f37c3
Ankit kumar gupta <ak0199374@gmail.com>
2025-05-25 13:33:25 +0530

    Update Pattern13.java

--- Pattern13.java
+++ Pattern13.java
@@ -3,7 +3,7 @@ public class Pattern13 {
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++)
             {
-                System.out.print(j);ddwwwwww
+                System.out.print(j);
 
             }
             System.out.println();
